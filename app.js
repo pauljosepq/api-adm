8636cb09cac8475def8e8a6313bfbd68d3533e11
Paul Paniagua <pauljosepq2@gmail.com>
2023-10-11 13:12:19 -0400

    ready

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const authRoutes = require('./controllers/authController');
 
 const app = express();
-
+app.use(cors())
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI).then(
   () => console.log(`Database ready`)
@@ -16,7 +17,7 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api/auth', authRoutes);
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
